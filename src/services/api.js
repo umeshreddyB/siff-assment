53b5e0c57a9fc2dcc98303328b0ccb528ec512c1
umeshreddy <umeshhoney7@gmail.com>
2026-01-20 13:09:02 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,3 @@
-import axios from 'axios'
-
-
-const API_BASE_URL = 'https://jsonplaceholder.typicode.com' 
-
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-
 
 const mockCourses = [
   {
